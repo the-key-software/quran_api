caae0965a9b721f605cbf4c077f43798c016d796
M Atheer <55885230+masreplay@users.noreply.github.com>
2024-02-09 18:08:35 +0300

    Add FilesClient and FilesClient.g.dart

---
+++ lib/src/clients/files_client.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_import
+
+import "package:dio/dio.dart";
+import "package:retrofit/retrofit.dart";
+
+import "../models/models.dart";
+
+part "files_client.g.dart";
+
+@RestApi(baseUrl: "https://files.quran.app/hafs/madani/width_1260")
+abstract class FilesClient {
+  factory FilesClient(Dio dio, {String baseUrl}) = _FilesClient;
+
+  @GET("/page{number}.png")
+  Future<HttpResponse<dynamic>> page({
+    // 001, 002, 003, ... 604
+    @Path("number") String number,
+  });
+}
